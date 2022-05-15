bb84023a838ab9eb8a594ec320671cb4a8551a3a
Papa Attrams <48452451+Attrams@users.noreply.github.com>
2022-05-15 05:45:36 +0000

    add the change_registration/2 and register_user/1

--- lib/rumbl/accounts.ex
+++ lib/rumbl/accounts.ex
@@ -28,4 +28,12 @@ defmodule Rumbl.Accounts do
   def create_user(attrs \\ %{}) do
     %User{} |> User.changeset(attrs) |> Repo.insert()
   end
+
+  def change_registration(%User{} = user, params) do
+    User.registration_changeset(user, params)
+  end
+
+  def register_user(attrs \\ %{}) do
+    %User{} |> User.registration_changeset(attrs) |> Repo.insert()
+  end
 end
