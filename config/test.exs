726ce8f454cddc31db19ef0deac4d65371519e11
Papa Attrams <48452451+Attrams@users.noreply.github.com>
2022-06-10 00:22:48 +0000

    set hashing rounds for testing

--- config/test.exs
+++ config/test.exs
@@ -28,3 +28,6 @@ config :logger, level: :warn
 
 # Initialize plugs at runtime for faster test compilation
 config :phoenix, :plug_init_mode, :runtime
+
+# Set number of hashing rounds
+config :pbkdf2_elixir, :rounds, 1
